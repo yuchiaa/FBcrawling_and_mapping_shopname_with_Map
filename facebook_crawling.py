d881fc0ce3b9c08645b7e6a07ed5a65701a3cf53
Lillian Cheng <li321100@gmail.com>
2020-12-22 19:13:50 +0800

    revision

--- facebook_crawling.py
+++ facebook_crawling.py
@@ -1,17 +1,17 @@
 import os
 import time
-import pandas as pd
 import random
+import pandas as pd
 from dotenv import load_dotenv
 from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 
-# change as needed
+### change as needed
 load_dotenv()
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
@@ -25,38 +25,34 @@ def FB_login(chromedriver_path,url):
     print('open')
     # driver.close()
 
-    # ------ login facebook ------
+    ### facebook login
     WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#email')))
-
     elem = driver.find_element_by_id("email")
     elem.send_keys(USERNAME)
-
     elem = driver.find_element_by_id("pass")
     elem.send_keys(PASSWORD)
-
     elem.send_keys(Keys.RETURN)
     time.sleep(5)
- 
-    driver.get(url) # Switch to the target group page
+    driver.get(url) # Switch to the group page
     return driver
 
 
 def FB_crawling(driver, n):
+    ### Scrolling the website
     for x in range(1,n):
-        print(x) # testing
+        #print(x) # testing
         driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
         t = random.choice([2,3,4,5]) # avoid being banned 
         time.sleep(t)
-
+        
+    ### POST Crawling
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     raw_data = soup.findAll('div', {'class':'story_body_container'})
-    #print(len(raw_data))
 
     posts = []
     for item in raw_data:
-        #see_more_txt = []
         description = item.select('p')
-        see_more = item.findAll('span', {'class': 'text_exposed_show'})
+        see_more = item.findAll('span', {'class': 'text_exposed_show'}) # Expand the 'See more' button
         if len(see_more) > 0:
             if len(description) > 0:
                 description = '\n'.join([d.text for d in description])
@@ -64,20 +60,18 @@ def FB_crawling(driver, n):
                 description = description + ' ' + see_more_txt 
             else:
                 description = '\n'.join([s.text for s in see_more])
-
         else:
             if len(description) > 0:
                 description = '\n'.join([d.text for d in description])
             else:
                 description = ''
 
-        # crawling post published time        
-        published = item.select('abbr')  # Get the formatted datetime of published
+        ### Post created time Crawling       
+        published = item.select('abbr')  # Get the formatted datetime of published time
         if published is not None:
             published = '\n'.join([pb.text for pb in published])
         else:
             published = ''
-
         post = {'Description': description, 'published': published}
         posts.append(post)
     return posts
@@ -100,7 +94,7 @@ def output_to_txt(posts, filename):
 '''def main():
     driver = FB_login(chromedriver_path, url)
     posts = FB_crawling(driver, 520)
-    output_to_csv(posts, '3_fb_crawling_output')
+    output_to_csv(posts, 'fb_crawling_output')
 
 
 if __name__ == '__main__':
